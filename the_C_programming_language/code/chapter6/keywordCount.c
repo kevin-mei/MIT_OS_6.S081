cfbc1a5f4947dd965c8bed19b91bfa18ecdc87b4
kevin-mei <824087038@qq.com>
2021-03-04 07:52:48 +0800

    更新lec2学习记录

--- the_C_programming_language/code/chapter6/keywordCount.c
+++ the_C_programming_language/code/chapter6/keywordCount.c
@@ -2,7 +2,7 @@
 #include <ctype.h>
 #include <string.h>
 #include "getch.c"
-#define MAXWORD 10
+#define MAXWORD 100
 #define NKEYS (sizeof keytab / sizeof(struct key))
 
 struct key
@@ -14,22 +14,46 @@ struct key
 int getword(char *, int);
 int binsearch(char *, struct key *, int); /* count C keywords */
 
-int main()
+int main_xiabiao()
 {
     int n;
     char word[MAXWORD];
+    int j =0;
     while (getword(word, MAXWORD) != EOF)
+    {
         if (isalpha(word[0]))
-            if ((n = binsearch(word, keytab, NKEYS)) >= 0)
+            if ((n = binsearch_xiabiao(word, keytab, NKEYS)) >= 0)
                 keytab[n].count++;
+        if(++j>9)
+            break;
+    }
+
     for (n = 0; n < NKEYS; n++)
         if (keytab[n].count > 0)
             printf("%4d %s\n", keytab[n].count, keytab[n].word);
     return 0;
-} 
+}
+
+int main()
+{
+    char word[MAXWORD];
+    struct key *p;
+    while(getword(word,MAXWORD)!=EOF)
+    {
+        if(isalpha(word[0]))
+            if((p = binserch(word,keytab,NKEYS)) !=NULL)
+                p->count++;
+    }
+    for (p = keytab; p < keytab + NKEYS; p++)
+    {
+        if (p->count > 0)
+            printf("%4d %s\n", p->count, p->word);
+    }
+    return 0;
+}
 
 /* binsearch:  find word in tab[0]...tab[n-1] */
-int binsearch(char *word, struct key tab[], int n)
+int binsearch_xiabiao(char *word, struct key tab[], int n)
 {
     int cond;
     int low, high, mid;
@@ -48,12 +72,36 @@ int binsearch(char *word, struct key tab[], int n)
     return -1;
 }
 
-/* getword:  get next word or character from input */ 
+struct key *
+binserch(char *word, struct key tab[], int n)
+{
+    int cond;
+    struct key *low = &tab[0];
+    //&tab[-1]和&tab[n]都超出了数组tab的范围。
+    // 前者是绝对非法的，而对后者的间接引用也是非法的。
+    // 但是，C语言的定义保证数组末尾之后的第一个元素（即&tab[n]）的指针算术运算可以正确执行
+    struct key *high = &tab[n];
+    struct key *mid;
+
+    while(low < high) // 用指针时，判断的是[low,high) 区间
+    {
+        mid = low+(high - low)/2;
+        if(cond = strcmp(word,mid->word) < 0)// word的首字母小于mid的首字母，word应在mid的前面
+            high=mid;
+        else if(cond >0)
+            low = mid+1;
+        else
+            return mid;
+    }
+    return NULL;
+}
+
+/* getword:  get next word or character from input */
 int getword(char *word, int lim)
 {
     int c;
     char *w = word;
-    while (isspace(c = getch())) //isspace:判断字符c是否为空格字符
+    while ((c = getch())) //isspace:判断字符c是否为空格字符
         ;
     if (c != EOF)
         *w++ = c;
@@ -63,11 +111,14 @@ int getword(char *word, int lim)
         return c;
     }
     for (; --lim > 0; w++)
-        if (!isalnum(*w = getch()))//判断字符变量c是否为字母或数字，若是则返回非零，否则返回零
+    {
+        if (!isalnum(*w = getch())) //判断字符变量c是否为字母或数字，若是则返回非零，否则返回零
         {
             ungetch(*w); // 读到的字符c不是字母或数字时，将它放回输入中
             break;
         }
+    }
+
     *w = '\0';
     return word[0];
 }
